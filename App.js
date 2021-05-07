08b3b74812d929751ad1ce76a99593721c413f44
Robert Jastrzębski <macbookpro@MacBook-Pro-Muza.local>
2021-05-07 12:51:34 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
+} from "react-native";
+import Products from "./compontents/Products";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [products, setProducts] = useState([]);
+
+  const handleAddProduct = () => {
+    Keyboard.dismiss();
+    setProducts([...products, text]);
+    setText(null);
+    console.log(...products, text);
+  };
+
+  const removeProduct = (index) => {
+    let itemsCopy = [...products];
+    itemsCopy.splice(index, 1);
+    setProducts(itemsCopy);
+    console.log(...itemsCopy);
+  };
+
+  const {
+    container,
+    wrapperList,
+    wrapperMainText,
+    textHeader,
+    items,
+    input,
+    addWrapper,
+    addText,
+    writeItemShopWrapper,
+  } = styles;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={container}>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View style={wrapperList}>
+          <View style={wrapperMainText}>
+            <Text style={textHeader}>Shopping List</Text>
+          </View>
+          <View style={items}>
+            {products.map((item, index) => {
+              return (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => removeProduct(index)}
+                >
+                  <Products text={item} />
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </View>
+      </ScrollView>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={writeItemShopWrapper}
+      >
+        <TextInput
+          style={input}
+          placeholder={"Add some products..."}
+          onChangeText={(text) => setText(text)}
+          value={text}
+        />
+        <TouchableOpacity onPress={() => handleAddProduct()}>
+          <View style={addWrapper}>
+            <Text style={addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -14,8 +91,54 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#e0ffff",
+  },
+  wrapperMainText: {
+    alignItems: "center",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    opacity: 0.8,
+  },
+  wrapperList: {
+    padding: 50,
+    paddingHorizontal: 20,
+  },
+  textHeader: {
+    fontSize: 24,
+    marginVertical: 10,
+    color: "green",
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 20,
+  },
+  writeItemShopWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addText: {
+    fontSize: 17,
   },
 });
---
+++ compontents/Products.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+
+const Products = (props) => {
+  const { containterItems, leftItem, itemText, icon, basket } = styles;
+  return (
+    <View style={containterItems}>
+      <View style={leftItem}>
+        <Entypo style={basket} name="shopping-basket" size={26} color="black" />
+        <Text style={itemText}>{props.text}</Text>
+      </View>
+      <MaterialIcons style={icon} name="delete" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  containterItems: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  textWrapper: {
+    justifyContent: "space-between",
+  },
+  leftItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  basket: {
+    opacity: 0.5,
+
+    marginRight: 20,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  icon: {
+    fontSize: 17,
+    opacity: 0.5,
+  },
+});
+
+export default Products;
